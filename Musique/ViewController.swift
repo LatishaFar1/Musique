1a9a8886e4122e9242abe848075d07d61d9f3da0
Tish Faroul <lfaroul14@gmail.com>
2022-08-08 22:28:54 -0400

    Initial Commit

---
+++ Musique/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musique
+//
+//  Created by Tish Faroul on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
